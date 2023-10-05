829dbde19ca2ae418ae76a34f156fde06e346341
FrancoCastillo99 <Francocast_99@hotmail.com>
2023-10-05 13:02:42 -0300

    Agregue comentarios, y TP1

--- ejercicio_en_clase_bucle_1.py
+++ ejercicio_en_clase_bucle_1.py
@@ -1,29 +1,32 @@
 import string 
 
-alphabet = list(string.ascii_lowercase)
+alphabet = list(string.ascii_lowercase) # Importo string y uso el metodo ascii_lowercase para guardar todas las letras del abecedario en una lista
 
 move = int(input("Ingrese la cantidad de lugares que quiere correr cada letra: "))
 
-word_list = []
-word_final = ""
-word_final_list = []
+word_list = [] # Creo la lista que contendra las palabras originales
+word_final = "" #Creo la variable donde voy a armar la palabra modificada
+word_final_list = [] #Creo la lista donde voy a guardar las palabras modificadas
 
 print("Ingrese el mensaje (las 5 palabras que desea encriptar) : ")
 
+#Ingreso las palabras y las agrago a la lista 
 for i in range(5):
     word = list(input().lower())
     word_list.append(word)
 
+#Recorro la lista 
 for m in word_list:
+    #Recorro la palabra
     for i in range(len(m)):
         char = m[i]
-        if char in alphabet:
-            n = (alphabet.index(char)+move) % 26
-            word_final += alphabet[n]
+        if char in alphabet: #Pregunto si la letra que saque de la palabra esta contenida en la lista alphabet
+            n = (alphabet.index(char)+move) % 26 #Creo el indice de la letra corrida
+            word_final += alphabet[n] #Agrego cada letra modificada a la varieble
         else:
-            word_final += char
+            word_final += char #Si el caracter no esta contenido en la lista alphabet guardo el mismo caracter que ya existia
     else:
-        word_final_list.append(word_final)   
-        word_final=""
+        word_final_list.append(word_final)   #Agrego la nueva palabra a la nueva lista
+        word_final="" #Vacio la variable donde se guardan las palabras modificadas
 
-print(word_final_list)
+print(word_final_list) #Muestro la lista con las palabras encriptadas
--- ejercicio_en_clase_bucle_2.py
+++ ejercicio_en_clase_bucle_2.py
@@ -1,9 +1,10 @@
-num_list= []
-par = 0
-impar = 0
-total_par = 0
-total_impar = 0
+num_list= [] #lista que va a contener los numeros ingresados
+par = 0 #contador de numeros par
+impar = 0 #contador de numeros impar
+total_par = 0 #contador de numero par totales
+total_impar = 0 #contador de numeros impar totales
 
+#bucle para ingresar numeros, cuando pido 0 corta
 while True:
     num = int(input("Ingrese un número (0 cuando desea terminar el ingreso): "))
     if num != 0:
@@ -11,19 +12,20 @@ while True:
     else:
         break
 
-for n in num_list:
-    n = str(n)
+
+for n in num_list: #bucle que recorre los elementos de la lista num_list
+    n = str(n) #convierto el numero el string para poder recorrerlo
     for i in n:
-        i = int(i)
-        if i % 2 == 0:
+        i = int(i) #convierto el string en int para poder realizar calculos matematicos
+        if i % 2 == 0: #pregunto si es par, y lo sumo en el contador, caso contrario lo hago en contador de impares
             par += 1
         else:
             impar += 1
-    new_par = par
-    new_impar = impar
-    total_par += par
-    total_impar += impar
-    par = 0
-    impar = 0
-    print(f"El número {n}, tiene {new_par} digitos par, y {new_impar} digitos impares")
-print(f"El total de digitos pares es {total_par}, y el total de impares es {total_impar}")
+    new_par = par #guardo el valor en otra variable para poder vaciar el contador
+    new_impar = impar #guardo el valor en otra variable para poder vaciar el contador
+    total_par += par #Sumo el total de digitos par 
+    total_impar += impar #Sumo el total de digitos impar
+    par = 0 #Vacio el contador de par
+    impar = 0 #Vacio el contador de impar
+    print(f"El número {n}, tiene {new_par} digitos par, y {new_impar} digitos impares") #Muestro cada número y su cantidad de números par e impar
+print(f"El total de digitos pares es {total_par}, y el total de impares es {total_impar}") #Muestro total de digitos par e impar
